0d78fe6fdfc7be82f244eccfdbdb3f76f74bb77d
infomalhaut <infomalhaut@gmail.com>
2022-08-16 10:57:10 +0530

    first commit

---
+++ src/main/java/com/example/webapp/WebappApplication.java
@@ -0,0 +1,20 @@
+package com.example.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello(){
+		return "Hello buddy";
+	}
+}
